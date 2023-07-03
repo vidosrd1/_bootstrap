d1b383228360c1e5eaf9c81a41f081968bb20f1a
Srđan Vidović <vidosrd@gmail.com>
2023-07-03 17:45:51 +0200

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :tags
   resources :novines
   resources :arts
   resources :lists
   resources :categories
   resources :blogs
-  root "blog_posts#index"
+  #root "blog_posts#index"
+  root "home#index"
   devise_for :users
   resources :blog_posts
   # root "articles#index"
